1783d9def73c7f27a85b7943c065ccd583737977
antonv4126 <anton@aa.xs4all.nl>
2021-02-20 16:40:36 +0100

    4 files modified

--- src/app/ChessBoard/chess.component.ts
+++ src/app/ChessBoard/chess.component.ts
@@ -8,4 +8,7 @@ import { Component, Input } from '@angular/core';
 
 export class ChessBoardComponent {
   @Input() id:  string;
+  @Input() headertxt:  string;
+  @Input() maintxt:  string;
+  @Input() tiletype: string;
  }
